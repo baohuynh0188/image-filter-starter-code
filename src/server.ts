a1e8238f907a2bef7e4fcb95f2954df620f5f920
Bao Huynh <baohuynh@iMac-Pro.local>
2023-03-02 22:23:13 +0700

    implement filtered image

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -29,18 +29,30 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   /**************************************************************************** */
 
+  app.get("/filteredimage/", async (req: Request, res: Response): Promise<Response|void> => {
+    const { image_url }: { image_url: string } = req.query;
+
+    if (!image_url) return res.status(400).send("Image URL is required");
+
+    const imagePath: string = await filterImageFromURL(image_url);
+
+    return res.status(200).sendFile(imagePath, async () => {
+      await deleteLocalFiles([imagePath]);
+    });
+  });
+
   //! END @TODO1
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
+  app.get("/", async (req: Request, res: Response): Promise<void> => {
+    res.send("try GET /filteredimage?image_url={{}}");
+  });
   
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log( `server running http://localhost:${ port }` );
+    console.log( `press CTRL+C to stop server` );
+  });
 })();
